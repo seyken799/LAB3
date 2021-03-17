f732c79bbc4b0d7d3ee5367efbf6bc6149f2a5aa
Blazej Jurczak <blazej7887@gmail.com>
2021-03-17 17:11:35 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+
+# import random
+#ZAD1
+# a = [1 -x for x in range(1, 10)]
+# print(a)
+#
+# b = [4**y for y in range(8)]
+# print(b)
+#
+# c = [x for x in b if x % 2 == 0]
+# print(c)
+
+#ZAD2
+#
+# lista1 = [random.randint(1, 100) for x in range(10)]
+# lista2 = [x for x in lista1 if x % 2 ==0]
+# print(lista1)
+# print(lista2)
+
+#ZAD3
+
+# produkty = {"jajka" : "sztuki",
+#             "bulki":"sztuki",
+#             "kielbasa":"kg",
+#             "ziemniaki":"kg",
+#             "woda":"litry",
+#             "sok":"litry"}
+# print(produkty)
+# lista_sztuki =  [x for x in produkty.keys() if produkty[x] =="sztuki"]
+# print(lista_sztuki)
+
+#ZAD4
+# def trojkat_prostokatny(a, b ,c):
+#     if a ** 2 + b ** 2 == c ** 2:
+#         return 'Jest prostokatny'
+#     else:
+#         return 'Nie jest'
+# print(trojkat_prostokatny(1,2,3))
+# print(trojkat_prostokatny(3,4,5))
+#ZAD5
+# def pole_trapezu(a=1,b=2,h=3):
+#     return ((a + b)* h) / 2
+# print(pole_trapezu())
+
+#ZAD6
+#ZAD7
+#ZAD8
+def zakupy(**produkty)
+#ZAD9
